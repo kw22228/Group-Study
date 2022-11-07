83124d00c01def651bbf038636002238c713a4d5
91savage <ekdma1403@gmail.com>
2022-11-08 00:16:00 +0900

    1108

--- baseballGame_sehun/src/lib/app.js
+++ baseballGame_sehun/src/lib/app.js
@@ -4,4 +4,16 @@ export default class App {
   constructor(rootId) {
     this.#rootElement = document.getElementById(rootId);
   }
+  // eslint-disable-next-line class-methods-use-this
+  template() {
+    return /* html */ `
+    <h1> 숫자 야구 게임 </h1>
+    <form id = "form">
+        <input type="text" id="input">
+
+    `;
+  }
+  render() {
+    this.#rootElement.innerHTML = this.template();
+  }
 }
