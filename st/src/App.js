0a4758b160adcdf8ca05bdfd71be89daa6c6c6d3
김재원 <kw2282222@gmail.com>
2023-01-25 23:29:28 +0900

    cra

---
+++ st/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TeaSet from './test/TestSet';
+
+function App() {
+  return (
+    <div>
+      <TeaSet />
+    </div>
+  );
+}
+
+export default App;
---
+++ st/src/test/TestSet.js
@@ -0,0 +1,18 @@
+let guest = 0;
+
+function Cup() {
+  console.log('cup');
+  // Bad: changing a preexisting variable!
+  guest = guest + 1;
+  return <h2>Tea cup for guest #{guest}</h2>;
+}
+
+export default function TeaSet() {
+  return (
+    <>
+      <Cup />
+      <Cup />
+      <Cup />
+    </>
+  );
+}
