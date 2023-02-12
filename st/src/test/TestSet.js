1e876602949619259bed33b4dc046af331b0f483
김재원 <kw2282222@gmail.com>
2023-02-12 19:12:25 +0900

    testing

--- st/src/test/TestSet.js
+++ st/src/test/TestSet.js
@@ -2,14 +2,18 @@ import { useEffect, useState } from 'react';
 
 export default function TeaSet() {
   const [count, setCount] = useState(0);
-  useEffect(() => {
-    let ignore = false;
-    console.log(ignore);
+  // useEffect(() => {
+  //   let ignore = false;
+  //   console.log(ignore);
 
-    return () => {
-      ignore = true;
-      console.log(ignore);
-    };
-  }, [count]);
+  //   return () => {
+  //     ignore = true;
+  //     console.log(ignore);
+  //   };
+  // }, [count]);
+
+  setCount(count);
+  setCount(prev => prev + 1);
+  setCount(count, () => setCount(count));
   return <button onClick={() => setCount(prev => prev + 1)}>{count}</button>;
 }
